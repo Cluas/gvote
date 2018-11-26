223151236027be19c405221f5cf0d4c2a6fd6111
Cluas <Cluas@live.cn>
2018-11-26 16:07:05 +0800

    认证装饰器异常处理

--- utils/decorators.py
+++ utils/decorators.py
@@ -2,6 +2,7 @@ import functools
 
 import jwt
 
+from models import objects
 from models.users import User
 from settings import public_key
 
@@ -26,15 +27,24 @@ def async_authenticated(func):
 
                 # 从数据库中获取到user并设置给_current_user
                 try:
-                    user = await self.application.objects.get(User.select(User).where(User.id == user_id))
+                    user = await objects.get(User.select(User).where(User.id == user_id))
                     self._current_user = user
 
                     # 此处很关键
                     await func(self, *args, **kwargs)
                 except User.DoesNotExist:
                     self.set_status(401)
+                    self.finish(
+                        dict(
+                            err_code='user_does_not_exist', err_msg='用户不存在'
+                        )
+                    )
             except jwt.ExpiredSignatureError:
-                self.set_status(401)
+                self.finish(
+                    dict(
+                        err_code='jwt_expire', err_msg='Token已过期'
+                    )
+                )
         else:
             self.set_status(401)
             self.finish(
@@ -59,4 +69,5 @@ def async_admin_required(func):
                     err_code='admin_required', err_msg='请使用管理员权限账号访问'
                 )
             )
+
     return wrapper
