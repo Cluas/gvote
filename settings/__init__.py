c86bd8d5b9407ac0606a55a78925ecf442c1683a
Cluas <Cluas@live.cn>
2018-11-26 16:07:32 +0800

    设置调整

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,8 +4,10 @@ import peewee_async
 import redis
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APPID = 'wx45973a96444c3cf2'
+SECRET_KEY = '3efebe11ba2097f52ea27a2f5f5e4752'
 settings = {
-    "static_path": "C:/projects/tornado_overview/chapter03/static",
+    "static_path": BASE_DIR + "/static",
     "static_url_prefix": "/static/",
     "template_path": "templates",
     "secret_key": "ZGGA#Mp4yL4w5CDu",
@@ -20,11 +22,12 @@ settings = {
         "port": 3306
     },
     "redis": {
-        "host": "127.0.0.1"
+        "host": "127.0.0.1",
+        'decode_responses': True
     },
     'wexin_oauth': {
-        'key': 'wx45973a96444c3cf2',
-        'secret': '3efebe11ba2097f52ea27a2f5f5e4752'
+        'key': APPID,
+        'secret': SECRET_KEY
     }
 }
 
@@ -33,3 +36,7 @@ database = peewee_async.MySQLDatabase(
 )
 
 redis = redis.StrictRedis(**settings["redis"])
+
+private_key = open(BASE_DIR + '/jwt-key').read()
+
+public_key = open(BASE_DIR + '/jwt-key.pub').read()
