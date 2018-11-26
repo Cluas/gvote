3f4e8fab5d436a16228f55d93a6aa278c4be41fb
Cluas <xcluas@gmail.com>
2018-11-26 16:14:27 +0800

    Update __init__.py

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,20 +4,20 @@ import peewee_async
 import redis
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-APPID = 'wx45973a96444c3cf2'
-SECRET_KEY = '3efebe11ba2097f52ea27a2f5f5e4752'
+APPID = '************'
+SECRET_KEY = '************'
 settings = {
     "static_path": BASE_DIR + "/static",
     "static_url_prefix": "/static/",
     "template_path": "templates",
-    "secret_key": "ZGGA#Mp4yL4w5CDu",
+    "secret_key": '************',
     "jwt_expire": 7 * 24 * 3600,
     "MEDIA_ROOT": os.path.join(BASE_DIR, "media"),
     "SITE_URL": "http://127.0.0.1:8888",
     "database": {
         "host": "127.0.0.1",
-        "user": "root",
-        "password": "root",
+        "user": "'************'",
+        "password": "'************'",
         "name": "message",
         "port": 3306
     },
@@ -32,7 +32,7 @@ settings = {
 }
 
 database = peewee_async.MySQLDatabase(
-    'vote', host="127.0.0.1", port=3306, user="root", password="imyuols123"
+    'vote', host="127.0.0.1", port=3306, user="************", password="************"
 )
 
 redis = redis.StrictRedis(**settings["redis"])
